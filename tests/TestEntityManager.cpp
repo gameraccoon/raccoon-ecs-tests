7b7abd31f7d98eacaeb5b7b46691f6ce02b18573
Pavel Grebnev <ruwhynot@gmail.com>
2021-05-11 22:51:20 +0200

    Simplified some tests

--- tests/TestEntityManager.cpp
+++ tests/TestEntityManager.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <functional>
+#include <vector>
 
 #include "raccoon-ecs/entity_manager.h"
 
@@ -87,8 +88,8 @@ TEST(EntityManager, EntitiesCanBeCreatedAndRemoved)
 	auto entityManagerData = prepareEntityManager();
 	EntityManager& entityManager = entityManagerData->entityManager;
 
-	Entity testEntity1 = entityManager.addEntity();
-	Entity testEntity2 = entityManager.addEntity();
+	const Entity testEntity1 = entityManager.addEntity();
+	const Entity testEntity2 = entityManager.addEntity();
 
 	EXPECT_NE(testEntity1, testEntity2);
 	EXPECT_NE(testEntity1.getId(), testEntity2.getId());
@@ -108,9 +109,9 @@ TEST(EntityManager, ComponentsCanBeAddedToEntities)
 	auto entityManagerData = prepareEntityManager();
 	EntityManager& entityManager = entityManagerData->entityManager;
 
-	TestVector2 location{1, 0};
+	const TestVector2 location{1, 0};
 
-	Entity testEntity = entityManager.addEntity();
+	const Entity testEntity = entityManager.addEntity();
 
 	{
 		TransformComponent* transform = entityManager.addComponent<TransformComponent>(testEntity);
@@ -119,7 +120,7 @@ TEST(EntityManager, ComponentsCanBeAddedToEntities)
 	}
 
 	{
-		auto [resultTransform] = entityManager.getEntityComponents<TransformComponent>(testEntity);
+		const auto [resultTransform] = entityManager.getEntityComponents<TransformComponent>(testEntity);
 		EXPECT_TRUE(location == resultTransform->pos);
 	}
 }
@@ -131,89 +132,57 @@ TEST(EntityManager, EntitesWithComponentsCanBeRemoved)
 	auto entityManagerData = prepareEntityManager();
 	EntityManager& entityManager = entityManagerData->entityManager;
 
-	TestVector2 location1{1, 0};
-	TestVector2 location2{0, 1};
-	TestVector2 location3{1, 1};
+	const auto checkOnlyLocationsExist = [&entityManager](std::vector<TestVector2> locations)
+	{
+		std::vector<std::tuple<TransformComponent*>> components;
+		entityManager.getComponents<TransformComponent>(components);
+		EXPECT_EQ(locations.size(), components.size());
 
-	Entity testEntity1 = entityManager.addEntity();
+		for (auto& [transform] : components)
+		{
+			const size_t oldSize = locations.size();
+			locations.erase(
+				std::remove(
+					locations.begin(),
+					locations.end(),
+					transform->pos),
+				locations.end()
+			);
+			// each iteration should remove exactly one element
+			EXPECT_EQ(oldSize, locations.size() + 1);
+		}
+		EXPECT_TRUE(locations.empty());
+	};
+
+	const TestVector2 location1{1, 0};
+	const TestVector2 location2{0, 1};
+	const TestVector2 location3{1, 1};
+
+	const Entity testEntity1 = entityManager.addEntity();
 	TransformComponent* transform1 = entityManager.addComponent<TransformComponent>(testEntity1);
 	transform1->pos = location1;
 
-	Entity testEntity2 = entityManager.addEntity();
+	const Entity testEntity2 = entityManager.addEntity();
 	TransformComponent* transform2 = entityManager.addComponent<TransformComponent>(testEntity2);
 	transform2->pos = location2;
 
-	std::vector<std::tuple<TransformComponent*>> components;
-	entityManager.getComponents<TransformComponent>(components);
-	EXPECT_EQ(static_cast<size_t>(2u), components.size());
-
-	bool location1Found = false;
-	bool location2Found = false;
-	for (auto& [transform] : components)
-	{
-		TestVector2 location = transform->pos;
-		if (location == location1 && location1Found == false)
-		{
-			location1Found = true;
-		}
-		else if (location == location2 && location2Found == false)
-		{
-			location2Found = true;
-		}
-		else
-		{
-			GTEST_FAIL();
-		}
-	}
-	EXPECT_TRUE(location1Found);
-	EXPECT_TRUE(location2Found);
+	checkOnlyLocationsExist({ location1, location2 });
 
 	entityManager.removeEntity(testEntity2);
 
-	Entity testEntity3 = entityManager.addEntity();
+	const Entity testEntity3 = entityManager.addEntity();
 	TransformComponent* transform3 = entityManager.addComponent<TransformComponent>(testEntity3);
 	transform3->pos = location3;
 
-	location1Found = false;
-	location2Found = false;
-	bool location3Found = false;
-	std::vector<std::tuple<TransformComponent*>> transforms;
-	entityManager.getComponents<TransformComponent>(transforms);
-	EXPECT_EQ(static_cast<size_t>(2u), transforms.size());
-	for (auto& [transform] : transforms)
-	{
-		TestVector2 location = transform->pos;
-		if (location == location1 && !location1Found)
-		{
-			location1Found = true;
-		}
-		else if (location == location2 && !location2Found)
-		{
-			location2Found = true;
-		}
-		else if (location == location3 && !location3Found)
-		{
-			location3Found = true;
-		}
-		else
-		{
-			GTEST_FAIL();
-		}
-	}
-	EXPECT_TRUE(location1Found);
-	EXPECT_FALSE(location2Found);
-	EXPECT_TRUE(location3Found);
-
-	entityManager.clearCaches();
-	transforms.clear();
-	entityManager.getComponents<TransformComponent>(transforms);
-	EXPECT_EQ(static_cast<size_t>(2u), transforms.size());
+	checkOnlyLocationsExist({ location1, location3 });
 
 	entityManager.removeEntity(testEntity3);
-	entityManager.clearCaches();
-	transforms.clear();
-	entityManager.getComponents<TransformComponent>(transforms);
-	EXPECT_EQ(static_cast<size_t>(1u), transforms.size());
+
+	checkOnlyLocationsExist({ location1 });
+
+	entityManager.removeEntity(testEntity1);
+
+	checkOnlyLocationsExist({});
 }
 
 TEST(EntityManager, ComponentsNeverCopiedOrMovedAndAlwaysDestroyed)
@@ -230,9 +199,9 @@ TEST(EntityManager, ComponentsNeverCopiedOrMovedAndAlwaysDestroyed)
 		auto entityManagerData = prepareEntityManager();
 		EntityManager& entityManager = entityManagerData->entityManager;
 
-		Entity testEntity1 = entityManager.addEntity();
-		Entity testEntity2 = entityManager.addEntity();
-		Entity testEntity3 = entityManager.addEntity();
+		const Entity testEntity1 = entityManager.addEntity();
+		const Entity testEntity2 = entityManager.addEntity();
+		const Entity testEntity3 = entityManager.addEntity();
 
 		{
 			LifetimeCheckerComponent* lifetimeChecker = entityManager.addComponent<LifetimeCheckerComponent>(testEntity1);

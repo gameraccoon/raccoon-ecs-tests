aa56af17e427f8f54fd3e20fda354831acc5f095
Pavel Grebnev <ruwhynot@gmail.com>
2021-05-12 22:22:19 +0200

    Added more entity manager tests

--- tests/TestEntityManager.cpp
+++ tests/TestEntityManager.cpp
@@ -7,18 +7,24 @@
 
 namespace ComponentTests
 {
-	enum ComponentType { TransformComponentId, MovementComponentId, LifetimeCheckerComponentId };
+	enum ComponentType { EmptyComponentId, TransformComponentId, MovementComponentId, LifetimeCheckerComponentId };
 	using ComponentFactory = RaccoonEcs::ComponentFactoryImpl<ComponentType>;
 	using EntityGenerator = RaccoonEcs::EntityGenerator;
 	using EntityManager = RaccoonEcs::EntityManagerImpl<ComponentType>;
 	using Entity = RaccoonEcs::Entity;
+	using TypedComponent = RaccoonEcs::TypedComponentImpl<ComponentType>;
 
 	struct TestVector2
 	{
 		int x;
 		int y;
 
-		bool operator==(TestVector2 other) { return x == other.x && y == other.y; }
+		bool operator==(TestVector2 other) const { return x == other.x && y == other.y; }
+	};
+
+	struct EmptyComponent
+	{
+		static ComponentType GetTypeId() { return EmptyComponentId; };
 	};
 
 	struct TransformComponent
@@ -71,12 +77,18 @@ namespace ComponentTests
 		EntityManager entityManager{componentFactory, entityGenerator};
 	};
 
-	static std::unique_ptr<EntityManagerData> prepareEntityManager()
+	static void RegisterComponents(ComponentFactory& inOutFactory)
+	{
+		inOutFactory.registerComponent<EmptyComponent>();
+		inOutFactory.registerComponent<TransformComponent>();
+		inOutFactory.registerComponent<MovementComponent>();
+		inOutFactory.registerComponent<LifetimeCheckerComponent>();
+	}
+
+	static std::unique_ptr<EntityManagerData> PrepareEntityManager()
 	{
 		auto data = std::make_unique<EntityManagerData>();
-		data->componentFactory.registerComponent<TransformComponent>();
-		data->componentFactory.registerComponent<MovementComponent>();
-		data->componentFactory.registerComponent<LifetimeCheckerComponent>();
+		RegisterComponents(data->componentFactory);
 		return data;
 	}
 }
@@ -85,7 +97,7 @@ TEST(EntityManager, EntitiesCanBeCreatedAndRemoved)
 {
 	using namespace ComponentTests;
 
-	auto entityManagerData = prepareEntityManager();
+	auto entityManagerData = PrepareEntityManager();
 	EntityManager& entityManager = entityManagerData->entityManager;
 
 	EXPECT_FALSE(entityManager.hasAnyEntities());
@@ -123,7 +135,7 @@ TEST(EntityManager, ComponentsCanBeAddedToEntities)
 {
 	using namespace ComponentTests;
 
-	auto entityManagerData = prepareEntityManager();
+	auto entityManagerData = PrepareEntityManager();
 	EntityManager& entityManager = entityManagerData->entityManager;
 
 	const TestVector2 location{1, 0};
@@ -146,7 +158,7 @@ TEST(EntityManager, EntitesWithComponentsCanBeRemoved)
 {
 	using namespace ComponentTests;
 
-	auto entityManagerData = prepareEntityManager();
+	auto entityManagerData = PrepareEntityManager();
 	EntityManager& entityManager = entityManagerData->entityManager;
 
 	const auto checkOnlyLocationsExist = [&entityManager](std::vector<TestVector2> locations)
@@ -213,7 +225,7 @@ TEST(EntityManager, ComponentsNeverCopiedOrMovedAndAlwaysDestroyed)
 	const auto copyFn = [&copiesCount]() { ++copiesCount; };
 
 	{
-		auto entityManagerData = prepareEntityManager();
+		auto entityManagerData = PrepareEntityManager();
 		EntityManager& entityManager = entityManagerData->entityManager;
 
 		const Entity testEntity1 = entityManager.addEntity();
@@ -250,4 +262,368 @@ TEST(EntityManager, ComponentsNeverCopiedOrMovedAndAlwaysDestroyed)
 	EXPECT_EQ(destroyedObjects[2], true);
 
 	EXPECT_EQ(copiesCount, 0);
+}
+
+TEST(EntityManager, ComponentSetsCanBeIteratedOver)
+{
+	using namespace ComponentTests;
+
+	auto entityManagerData = PrepareEntityManager();
+	EntityManager& entityManager = entityManagerData->entityManager;
+
+	const Entity testEntity1 = entityManager.addEntity();
+	entityManager.addComponent<TransformComponent>(testEntity1);
+	entityManager.addComponent<MovementComponent>(testEntity1);
+
+	const Entity testEntity2 = entityManager.addEntity();
+	entityManager.addComponent<TransformComponent>(testEntity2);
+	entityManager.addComponent<EmptyComponent>(testEntity2);
+
+	{
+		int iterationsCount = 0;
+		entityManager.forEachComponentSet<MovementComponent>(
+			[&iterationsCount](MovementComponent*)
+		{
+			++iterationsCount;
+		});
+		EXPECT_EQ(1, iterationsCount);
+	}
+
+	{
+		int iterationsCount = 0;
+		entityManager.forEachComponentSet<TransformComponent>(
+			[&iterationsCount](TransformComponent*)
+		{
+			++iterationsCount;
+		});
+		EXPECT_EQ(2, iterationsCount);
+	}
+
+	{
+		int iterationsCount = 0;
+		entityManager.forEachComponentSet<EmptyComponent, TransformComponent>(
+			[&iterationsCount](EmptyComponent*, TransformComponent*)
+		{
+			++iterationsCount;
+		});
+		EXPECT_EQ(1, iterationsCount);
+	}
+}
+
+TEST(EntityManager, ComponentSetsCanBeIteratedOverWithEntities)
+{
+	using namespace ComponentTests;
+
+	auto entityManagerData = PrepareEntityManager();
+	EntityManager& entityManager = entityManagerData->entityManager;
+
+	const Entity testEntity1 = entityManager.addEntity();
+	entityManager.addComponent<TransformComponent>(testEntity1);
+	entityManager.addComponent<MovementComponent>(testEntity1);
+
+	const Entity testEntity2 = entityManager.addEntity();
+	entityManager.addComponent<TransformComponent>(testEntity2);
+	entityManager.addComponent<EmptyComponent>(testEntity2);
+
+	{
+		int iterationsCount = 0;
+		entityManager.forEachComponentSetWithEntity<MovementComponent>(
+			[&iterationsCount, testEntity1](Entity entity, MovementComponent*)
+		{
+			EXPECT_EQ(testEntity1, entity);
+			++iterationsCount;
+		});
+		EXPECT_EQ(1, iterationsCount);
+	}
+
+	{
+		int iterationsCount = 0;
+		entityManager.forEachComponentSetWithEntity<TransformComponent>(
+			[&iterationsCount](Entity, TransformComponent*)
+		{
+			++iterationsCount;
+		});
+		EXPECT_EQ(2, iterationsCount);
+	}
+
+	{
+		int iterationsCount = 0;
+		entityManager.forEachComponentSetWithEntity<EmptyComponent, TransformComponent>(
+			[&iterationsCount, testEntity2](Entity entity, EmptyComponent*, TransformComponent*)
+		{
+			EXPECT_EQ(testEntity2, entity);
+			++iterationsCount;
+		});
+		EXPECT_EQ(1, iterationsCount);
+	}
+}
+
+TEST(EntityManager, ComponentSetsCanBeCollected)
+{
+	using namespace ComponentTests;
+
+	auto entityManagerData = PrepareEntityManager();
+	EntityManager& entityManager = entityManagerData->entityManager;
+
+	const Entity testEntity1 = entityManager.addEntity();
+	entityManager.addComponent<TransformComponent>(testEntity1);
+	entityManager.addComponent<MovementComponent>(testEntity1);
+
+	const Entity testEntity2 = entityManager.addEntity();
+	entityManager.addComponent<TransformComponent>(testEntity2);
+	entityManager.addComponent<EmptyComponent>(testEntity2);
+
+	{
+		std::vector<std::tuple<MovementComponent*>> components;
+		entityManager.getComponents<MovementComponent>(components);
+		EXPECT_EQ(static_cast<size_t>(1u), components.size());
+	}
+
+	{
+		std::vector<std::tuple<TransformComponent*>> components;
+		entityManager.getComponents<TransformComponent>(components);
+		EXPECT_EQ(static_cast<size_t>(2u), components.size());
+	}
+
+	{
+		std::vector<std::tuple<EmptyComponent*, TransformComponent*>> components;
+		entityManager.getComponents<EmptyComponent, TransformComponent>(components);
+		EXPECT_EQ(static_cast<size_t>(1u), components.size());
+	}
+}
+
+TEST(EntityManager, ComponentSetsWithEntitiesCanBeCollected)
+{
+	using namespace ComponentTests;
+
+	auto entityManagerData = PrepareEntityManager();
+	EntityManager& entityManager = entityManagerData->entityManager;
+
+	const Entity testEntity1 = entityManager.addEntity();
+	entityManager.addComponent<TransformComponent>(testEntity1);
+	entityManager.addComponent<MovementComponent>(testEntity1);
+
+	const Entity testEntity2 = entityManager.addEntity();
+	entityManager.addComponent<TransformComponent>(testEntity2);
+	entityManager.addComponent<EmptyComponent>(testEntity2);
+
+	{
+		std::vector<std::tuple<Entity, MovementComponent*>> components;
+		entityManager.getComponentsWithEntities<MovementComponent>(components);
+		EXPECT_EQ(static_cast<size_t>(1u), components.size());
+		if (!components.empty())
+		{
+			EXPECT_EQ(testEntity1, std::get<0>(components[0]));
+		}
+	}
+
+	{
+		std::vector<std::tuple<Entity, TransformComponent*>> components;
+		entityManager.getComponentsWithEntities<TransformComponent>(components);
+		EXPECT_EQ(static_cast<size_t>(2u), components.size());
+		if (components.size() >= 2)
+		{
+			EXPECT_NE(std::get<0>(components[0]), std::get<0>(components[1]));
+		}
+	}
+
+	{
+		std::vector<std::tuple<Entity, EmptyComponent*, TransformComponent*>> components;
+		entityManager.getComponentsWithEntities<EmptyComponent, TransformComponent>(components);
+		EXPECT_EQ(static_cast<size_t>(1u), components.size());
+		if (!components.empty())
+		{
+			EXPECT_EQ(testEntity2, std::get<0>(components[0]));
+		}
+	}
+}
+
+TEST(EntityManager, EntitiesCanBeMatchedByHavingComponents)
+{
+	using namespace ComponentTests;
+
+	auto entityManagerData = PrepareEntityManager();
+	EntityManager& entityManager = entityManagerData->entityManager;
+
+	const Entity testEntity1 = entityManager.addEntity();
+	entityManager.addComponent<TransformComponent>(testEntity1);
+	entityManager.addComponent<MovementComponent>(testEntity1);
+
+	const Entity testEntity2 = entityManager.addEntity();
+	entityManager.addComponent<TransformComponent>(testEntity2);
+	entityManager.addComponent<EmptyComponent>(testEntity2);
+
+	// check that components assigned correctly (way 1)
+	EXPECT_TRUE(entityManager.doesEntityHaveComponent<TransformComponent>(testEntity1));
+	EXPECT_TRUE(entityManager.doesEntityHaveComponent<MovementComponent>(testEntity1));
+	EXPECT_FALSE(entityManager.doesEntityHaveComponent<EmptyComponent>(testEntity1));
+	EXPECT_TRUE(entityManager.doesEntityHaveComponent<TransformComponent>(testEntity2));
+	EXPECT_FALSE(entityManager.doesEntityHaveComponent<MovementComponent>(testEntity2));
+	EXPECT_TRUE(entityManager.doesEntityHaveComponent<EmptyComponent>(testEntity2));
+	// check that components assigned correctly (way 2)
+	EXPECT_TRUE(entityManager.doesEntityHaveComponent(testEntity1, TransformComponentId));
+	EXPECT_TRUE(entityManager.doesEntityHaveComponent(testEntity1, MovementComponentId));
+	EXPECT_FALSE(entityManager.doesEntityHaveComponent(testEntity1, EmptyComponentId));
+	EXPECT_TRUE(entityManager.doesEntityHaveComponent(testEntity2, TransformComponentId));
+	EXPECT_FALSE(entityManager.doesEntityHaveComponent(testEntity2, MovementComponentId));
+	EXPECT_TRUE(entityManager.doesEntityHaveComponent(testEntity2, EmptyComponentId));
+
+	// use getEntitiesHavingComponents with one component matching one entity
+	{
+		std::vector<Entity> matchedEntities;
+		entityManager.getEntitiesHavingComponents({MovementComponentId}, matchedEntities);
+		EXPECT_EQ(static_cast<size_t>(1), matchedEntities.size());
+		if (!matchedEntities.empty())
+		{
+			EXPECT_EQ(testEntity1, matchedEntities[0]);
+		}
+	}
+
+	// use getEntitiesHavingComponents with one component matching two entities
+	{
+		std::vector<Entity> matchedEntities;
+		entityManager.getEntitiesHavingComponents({TransformComponentId}, matchedEntities);
+		EXPECT_EQ(static_cast<size_t>(2), matchedEntities.size());
+		if (matchedEntities.size() >= 2)
+		{
+			EXPECT_NE(testEntity1, testEntity2);
+		}
+	}
+
+	// use getEntitiesHavingComponents with two components matching one entity
+	{
+		std::vector<Entity> matchedEntities;
+		entityManager.getEntitiesHavingComponents({EmptyComponentId, TransformComponentId}, matchedEntities);
+		EXPECT_EQ(static_cast<size_t>(1), matchedEntities.size());
+		if (!matchedEntities.empty())
+		{
+			EXPECT_EQ(testEntity2, matchedEntities[0]);
+		}
+	}
+}
+
+TEST(EntityManager, AllCopmonentsFromOneEntityCanBeCollected)
+{
+	using namespace ComponentTests;
+
+	auto entityManagerData = PrepareEntityManager();
+	EntityManager& entityManager = entityManagerData->entityManager;
+
+	const Entity testEntity = entityManager.addEntity();
+	entityManager.addComponent<TransformComponent>(testEntity);
+	entityManager.addComponent<MovementComponent>(testEntity);
+
+	std::vector<TypedComponent> componentsWithTypes;
+	entityManager.getAllEntityComponents(testEntity, componentsWithTypes);
+	EXPECT_EQ(static_cast<size_t>(2), componentsWithTypes.size());
+	if (componentsWithTypes.size() >= 2)
+	{
+		if (componentsWithTypes[0].typeId == TransformComponentId)
+		{
+			EXPECT_EQ(MovementComponentId, componentsWithTypes[1].typeId);
+		}
+		else
+		{
+			EXPECT_EQ(MovementComponentId, componentsWithTypes[0].typeId);
+			EXPECT_EQ(TransformComponentId, componentsWithTypes[1].typeId);
+		}
+	}
+}
+
+TEST(EntityManager, ComponentAdditionOrRemovementCanBeScheduled)
+{
+	using namespace ComponentTests;
+
+	auto entityManagerData = PrepareEntityManager();
+	EntityManager& entityManager = entityManagerData->entityManager;
+
+	const Entity testEntity = entityManager.addEntity();
+	entityManager.addComponent<TransformComponent>(testEntity);
+
+	entityManager.forEachComponentSetWithEntity<TransformComponent>([&entityManager](Entity entity,TransformComponent*)
+	{
+		entityManager.scheduleRemoveComponent<TransformComponent>(entity);
+		MovementComponent* movement = entityManager.scheduleAddComponent<MovementComponent>(entity);
+		movement->move = TestVector2(2, 3);
+	});
+
+	entityManager.executeScheduledActions();
+
+	EXPECT_FALSE(entityManager.doesEntityHaveComponent<TransformComponent>(testEntity));
+	EXPECT_TRUE(entityManager.doesEntityHaveComponent<MovementComponent>(testEntity));
+	auto [movement] = entityManager.getEntityComponents<MovementComponent>(testEntity);
+	EXPECT_NE(nullptr, movement);
+	if (movement != nullptr)
+	{
+		EXPECT_EQ(TestVector2(2, 3), movement->move);
+	}
+}
+
+TEST(EntityManager, EntitiesCanBeTransferedBetweenCopmonents)
+{
+	using namespace ComponentTests;
+
+	EntityGenerator entityGenerator(42);
+	ComponentFactory componentFactory;
+	RegisterComponents(componentFactory);
+	EntityManager entityManager1(componentFactory, entityGenerator);
+
+	const Entity testEntity = entityManager1.addEntity();
+	{
+		TransformComponent* transform = entityManager1.addComponent<TransformComponent>(testEntity);
+		transform->pos = TestVector2(10, 3);
+		entityManager1.addComponent<MovementComponent>(testEntity);
+	}
+
+	EntityManager entityManager2(componentFactory, entityGenerator);
+	entityManager1.transferEntityTo(entityManager2, testEntity);
+
+	EXPECT_FALSE(entityManager1.hasEntity(testEntity));
+	EXPECT_TRUE(entityManager2.hasEntity(testEntity));
+	EXPECT_TRUE(entityManager2.doesEntityHaveComponent<TransformComponent>(testEntity));
+	EXPECT_TRUE(entityManager2.doesEntityHaveComponent<MovementComponent>(testEntity));
+	auto [transform] = entityManager2.getEntityComponents<TransformComponent>(testEntity);
+	if (transform != nullptr)
+	{
+		EXPECT_EQ(TestVector2(10, 3), transform->pos);
+	}
+}
+
+
+TEST(EntityManager, EntityCanBeAddedInTwoSteps)
+{
+	using namespace ComponentTests;
+
+	auto entityManagerData = PrepareEntityManager();
+	EntityManager& entityManager = entityManagerData->entityManager;
+
+	Entity testEntity = entityManager.getNonExistentEntity();
+
+	auto doRedoCommand = [testEntity, &entityManager]()
+	{
+		entityManager.tryInsertEntity(testEntity);
+		entityManager.addComponent<TransformComponent>(testEntity);
+	};
+
+	auto undoCommand = [testEntity, &entityManager]()
+	{
+		entityManager.removeEntity(testEntity);
+	};
+
+	EXPECT_FALSE(entityManager.hasAnyEntities());
+	EXPECT_FALSE(entityManager.hasEntity(testEntity));
+
+	doRedoCommand();
+
+	EXPECT_TRUE(entityManager.hasEntity(testEntity));
+	EXPECT_TRUE(entityManager.doesEntityHaveComponent<TransformComponent>(testEntity));
+
+	undoCommand();
+
+	EXPECT_FALSE(entityManager.hasEntity(testEntity));
+
+	doRedoCommand();
+
+	EXPECT_TRUE(entityManager.hasEntity(testEntity));
+	EXPECT_TRUE(entityManager.doesEntityHaveComponent<TransformComponent>(testEntity));
 }
